990d5b3bb78ba59dd414f9440b4718dadbcd859f
webchick <332535+webchick@users.noreply.github.com>
2023-06-17 00:21:08 -0700

    Printing to console :)

--- Assets/PrintToConsole.cs
+++ Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("I'm printing to the console!"); 
     }
 
     // Update is called once per frame
